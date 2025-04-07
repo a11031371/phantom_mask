bed153337b1bb3630547d7af0916a6cdd47d1f19
a11031371 <a11031371@gmail.com>
2025-04-07 16:16:14 +0800

    Set debug false

--- phantom_mask_api_server/phantom_mask_api_server/settings.py
+++ phantom_mask_api_server/phantom_mask_api_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%sv@98n$i*4o0r622ce-p4-rla=ee@jx8ce0yw0$jes=dp7vlw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
