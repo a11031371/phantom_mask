706b3ce01cdc1481889008114697402dc4ec4442
a11031371 <a11031371@gmail.com>
2025-04-08 17:59:08 +0800

    adjust settings

--- phantom_mask_api_server/phantom_mask_api_server/settings.py
+++ phantom_mask_api_server/phantom_mask_api_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%sv@98n$i*4o0r622ce-p4-rla=ee@jx8ce0yw0$jes=dp7vlw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # allowed hosts in production
 ALLOWED_HOSTS = ['localhost', 'phantom-mask-production-b908.up.railway.app', '0.0.0.0', '127.0.0.1']
